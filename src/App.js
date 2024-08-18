ccc7cc0a2417d5b684dd2794dbdf9c8fc45ae2dd
integralnana <nwpscw79@gmail.com>
2024-08-18 18:25:58 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import react from 'react'
-import {Header,Content,Footer,} from './func-component';
+import React from "react";
+import {Calculator} from "./calculator";
 
-
-
-function App(){
-  return(
-    [<Header/>,<Content/>,<Footer/>]
-
-  
-  )
+function App() {
+  return <Calculator/>;
 }
 export default App;
--- src/class_component.js
+++ src/class_component.js
@@ -1,19 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default class Calender extends React.Component{
-
-    getDate(){
-    const dayNames = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-    const monthNames = ['January','February','March','April','May','June','july','August','September','October','November','December']
-    const date = new Date()
-    const weekDay = dayNames[date.getDay()]
-    const day = date.getDate()
-    const month = monthNames[date.getMonth()]
-    const year = date.getFullYear()
-    return `${weekDay} ${day} ${month} ${year}`
-}
-
-render(){
-    return <div>{this.getDate()}</div>
-    }
+export default class Button extends Component {
+  showAlert(msg) {
+    alert(msg);
+  }
+  onClickButtonOK = () => {
+    this.showAlert("Hello");
+  };
+  render() {
+    return <button onClick={this.onClickButtonOK}>OK</button>;
+  }
 }
